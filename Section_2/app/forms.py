d486a5e60d49bfab04ce0047c532662b0026f06e
MichaelPotishman <146711713+MichaelPotishman@users.noreply.github.com>
2024-10-31 00:45:45 +0000

    nearly done

--- Section_2/app/forms.py
+++ Section_2/app/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, DateField, TextAreaField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class AssessmentForm(FlaskForm):
-    assessmenttitle = StringField('assessmenttitle', validators=[DataRequired()])
-    modulecode = StringField('modulecode', validators=[DataRequired()])
+    assessmenttitle = StringField('assessmenttitle', validators=[DataRequired(), Length(max=300)])
+    modulecode = StringField('modulecode', validators=[DataRequired(), Length(max=100)])
     deadline = DateField('deadline', validators=[DataRequired()])
-    description = TextAreaField('description', validators=[DataRequired()])
+    description = TextAreaField('description', validators=[DataRequired(), Length(max=500)])
     status = BooleanField('status')
 
 class NewPropertyForm(FlaskForm):
--- Section_2/app/views.py
+++ Section_2/app/views.py
@@ -1,36 +1,28 @@
 from app import app, db, models
 from .forms import NewPropertyForm, AssessmentForm
 from flask import render_template, flash, redirect
+from datetime import datetime
 
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     return render_template('home.html', assessments = models.Assessments.query.all())
 
-@app.route('/details', methods=['GET', 'POST'])
-def showDetails():
-    return render_template('details.html', objects = models.Assessments.query.all())
-
-@app.route('/clear', methods=['POST'])
-def clear():
-    db.session.query(models.Property).delete()
-    db.session.commit()
-    return "all assessments cleared"
-
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     form = AssessmentForm()
+    today = datetime.today().date()
     if form.validate_on_submit():
         flash("Successfully recieved data. %s" %(form.assessmenttitle.data))
         data = models.Assessments(assessmenttitle = form.assessmenttitle.data, modulecode = form.modulecode.data, deadline = form.deadline.data, description = form.description.data, status = form.status.data)
         db.session.add(data)
         db.session.commit()
         # return redirect('/details')
-    return render_template('add.html', form=form)
+    return render_template('add.html', form=form, today=today)
 
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    assessment = models.Assessments.query.get(id)
+@app.route('/edit/<int:item_id>', methods=['GET', 'POST'])
+def edit_item(item_id):
+    assessment = models.Assessments.query.get(item_id)
     form = AssessmentForm(obj=assessment)
     if form.validate_on_submit():
         assessment.assessmenttitle = form.assessmenttitle.data
@@ -46,6 +38,31 @@ def edit(id):
         db.session.commit()
 
     return render_template('edit.html', form=form, assessment=assessment)
+
+@app.route('/delete/<int:item_id>', methods=['POST'])
+def delete_item(item_id):
+    item = models.Assessments.query.get(item_id)
+    if item:
+        db.session.delete(item)
+        db.session.commit()
+    return redirect('/')
+
+@app.route('/complete_item/<int:item_id>', methods=['POST'])
+def complete_item(item_id):
+    item = models.Assessments.query.get(item_id)
+    if item:
+        item.status = 1 
+        db.session.commit()
+    return redirect('/')  
+
+@app.route('/uncomplete_item/<int:item_id>', methods=['POST'])
+def uncomplete_item(item_id):
+    item = models.Assessments.query.get(item_id)
+    if item:
+        item.status = 0 
+        db.session.commit()
+    return redirect('/')  
+
 
 
 
